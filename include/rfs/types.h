cc021fe686bb5cc59276905dc912fb84726d1459
Robert Robinson <r@faltung.ca>
2014-11-17 18:59:57 -0500

    The 9p wire protocol
    * Structure to hold requests
    * Functions to serialize and deserialize requests
    * Basic testing of said functions.
    * TODO: strengthen parsing code (it is pretty weak right now)

---
+++ include/rfs/types.h
@@ -0,0 +1,87 @@
+#ifndef RFS_TYPES_H
+#define RFS_TYPES_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+/// @brief These enum values describe the possible bitmask values of
+/// the 'type' field in a QID.
+enum {
+  RFS_QTDIR = 0x80, ///< This is a directory.
+  RFS_QTAPPEND = 0x40, ///< This file is append-only.
+  RFS_QTEXCL = 0x20, ///< The file is is marked as exclusive use only.
+  RFS_QTMOUNT = 0x10, ///< This is a mounted channel.
+  RFS_QTAUTH = 0x08, ///< This is an authentication file.
+  RFS_QTTMP = 0x04, ///< This file is not backed up.
+  RFS_QTFILE = 0x00 ///< Regular file.
+};
+
+/// @brief An ID which is unique across all files on a server.
+/// man 2 stat for more details.
+typedef struct rfs_qid {
+  /// @brief Each file has a path, which is unique per server.
+  /// It will not change, regardless of moves or renames.
+  /// 2 files are only the same if their paths are equal.
+  uint64_t path;
+
+  /// @brief The current version of the file.
+  uint32_t vers;
+
+  /// @brief The type of this file, see the RFS_QT* values.
+  uint8_t type;
+} rfs_qid_t;
+
+// These are not in an enum because RFS_DMDIR is too large to fit in an int,
+// which is the assumed size of an enum in C.
+
+/// @brief The entity is a directory.
+#define  RFS_DMDIR        = 0x80000000,
+/// @brief The entity is an append-only file.
+#define  RFS_DMAPPEND     = 0x40000000,
+/// @brief The entity is an exclusive use file.
+#define  RFS_DMEXCL       = 0x20000000,
+/// @brief The entity is a mounted channel.
+#define  RFS_DMMOUNT      = 0x10000000,
+/// @brief The entity is an authentication file.
+#define  RFS_DMAUTH       = 0x08000000,
+/// @brief The entity is not backed up.
+#define  RFS_DMTMP        = 0x04000000,
+/// @brief The read permission bit.
+#define  RFS_DMREAD       = 0x4,
+/// @brief The write permission bit..
+#define  RFS_DMWRITE      = 0x2,
+/// @brief The execute permission bit.
+#define  RFS_DMEXEC       = 0x1
+
+/// @brief A directory entity. man 2 stat for more details.
+typedef struct rfs_dirent {
+  // These fields don't relate directly to the dirent.
+  uint16_t type; ///< The type of the server hosting this file.
+  uint32_t dev; ///< The subtype of the server hosting this file.
+
+  // These fields are file-specific.
+  /// @brief The unique ID of the dirent on the server.
+  rfs_qid_t qid;
+  /// @brief The bitwise OR of RFS_DM* values defined for this dirent.
+  uint32_t mode;
+  /// @brief The last time this dirent was accessed. Seconds since epoch.
+  uint32_t atime;
+  /// @brief The last time this dirent was modified. Seconds since epoch.
+  uint32_t mtime;
+  /// @brief The length of the dirent.
+  /// 0 for directories.
+  /// Number of bytes in a file for files.
+  /// Number of bytes to read before blocking for streams.
+  uint64_t length;
+  /// @brief The last element of the path.
+  char* name;
+  /// @brief The name of the owner of the file.
+  char* uid;
+  /// @brief The name of the group of the file.
+  char* gid;
+  /// @brief Name of the user who last modified the file (i.e. updated mtime).
+  char* muid;
+} rfs_dirent_t;
+
+#endif
+
---
+++ src/rfs_9p_wire.h
@@ -0,0 +1,292 @@
+
+#ifndef RFS_PROTO_9P
+#define RFS_PROTO_9P
+
+#include "rfs/types.h"
+
+/// @file The Plan 9 wire protocol functions.
+/// The types and functions to serialize and deserialize P9 messages.
+/// This should not be used directly, it is wrapped by rfs_9p.h which exposes
+/// more user-friendly functions. 
+///
+/// Per the 9P spec, all multi-byte integer values are encoded as little
+/// endian values. Strings are prefixed with a 2-byte size, and strings
+/// are not null terminated.
+///
+/// @todo Greatly improve the implementation of the pack/unpack functions.
+/// They assert when they should return errors, they are likely vulnerable
+/// to overflow attacks, etc. This implementation is the bare minimum
+/// for a trusted, development network, not the real world.
+
+/// @brief Per Plan9's fcall.h, R-types are one greater than their T-types.
+enum {
+  RFS__9P_TVERSION = 100,
+  RFS__9P_RVERSION,
+
+  RFS__9P_TAUTH = 102,
+  RFS__9P_RAUTH,
+
+  RFS__9P_TERROR = 106, // invalid, defined for consistency with fcall.h
+  RFS__9P_RERROR,
+
+  RFS__9P_TFLUSH = 108,
+  RFS__9P_RFLUSH,
+
+  RFS__9P_TATTACH = 104,
+  RFS__9P_RATTACH,
+
+  RFS__9P_TWALK = 110,
+  RFS__9P_RWALK,
+
+  RFS__9P_TOPEN = 112,
+  RFS__9P_ROPEN,
+
+  RFS__9P_TCREATE = 114,
+  RFS__9P_RCREATE,
+
+  RFS__9P_TREAD = 116,
+  RFS__9P_RREAD,
+
+  RFS__9P_TWRITE = 118,
+  RFS__9P_RWRITE,
+
+  RFS__9P_TCLUNK = 120,
+  RFS__9P_RCLUNK,
+
+  RFS__9P_TREMOVE = 122,
+  RFS__9P_RREMOVE,
+
+  RFS__9P_TSTAT = 124,
+  RFS__9P_RSTAT,
+
+  RFS__9P_TWSTAT = 126,
+  RFS__9P_RWSTAT
+};
+
+/// @brief The maximum number of values to return in 1 walk request
+#define RFS__9P_MAXWELEM          16
+
+/// @brief An invalid tag
+#define RFS__9P_NOTAG             (uint16_t) ~0U
+
+/// @brief An invalid fid
+#define RFS__9P_NOFID             (uint32_t) ~0U
+
+/// @brief The struct which stat data will be serialized to/from.
+/// Users transmitting this field should fill in everything except for
+/// size (which will be overwritten during serialization), then call
+/// rfs__9p_stat_pack() to serialize this object.
+/// Users receiving this field should obtain the buffer, then call
+/// rfs__9p_stat_unpack() to deserialize the buffer into this struct.
+///
+/// See man 5 stat for additional details.
+typedef struct rfs__9p_stat {
+  uint16_t size; ///< Total size of the stat, this field included.
+  uint16_t type; ///< The server type.
+  uint32_t dev; ///< The server sub-type.
+  rfs_qid_t qid; ///< The unique identifier of this entity on the server.
+  uint32_t mode; ///< The permission bits and flags.
+  uint32_t atime; ///< The last access time, seconds since epoch.
+  uint32_t mtime; ///< The last modification time, seconds since epoch.
+  uint64_t length; ///< The length of the file, in bytes.
+  char* name; ///< The last entry in the path. Must be / for root directories.
+  char* uid; ///< Name of the owner.
+  char* gid; ///< Name of the group.
+  char* muid; ///< Name of the last user to modify the file.
+} rfs__9p_stat_t;
+
+/// @brief The structure which 9P messages will be serialized to/from.
+/// Users transmitting this field should fill in the type, and the appropriate
+/// param union value, prior to calling rfs__9p_msg_pack() to serialize it.
+/// Users receiving this field should obtain the buffer, then call
+/// rfs__9p_msg_unpack() to deserialize the message into this struct.
+///
+/// See man 5 intro and man 2 fcall for more details.
+typedef struct rfs__9p_msg {
+  /// @brief The size of the message, this field included.
+  uint32_t size;
+
+  /// @brief The type of this message. See RFS__9P* for valid values.
+  uint8_t type;
+
+  /// @brief The identifying tag of this message.
+  uint16_t tag;
+
+  union {
+    struct {
+      uint32_t msize; ///< The maximum supported size of the message.
+      char* version; ///< The version string.
+    } version; ///< The content of the T and Rversion messages.
+
+    struct {
+      uint32_t afid; ///< The authentication fid.
+      char* uname; ///< The username of the auth request.
+      char* aname; ///< The name of the file tree to be authenticating to.
+    } tauth; ///< The content of the Tauth message.
+
+    struct {
+      rfs_qid_t aqid; ///< The qid of the authentication file.
+    } rauth; ///< The content of the Rauth message.
+
+    struct {
+      char* ename; ///< The error string describing why a request failed.
+    } rerror; ///< The content of the Rerror message.
+
+    struct {
+      uint16_t oldtag; ///< The tag of the message to abort.
+    } tflush; ///< The content of the Tflush message.
+
+    // rflush is empty.
+
+    struct {
+      uint32_t fid; ///< The fid to save as the root of the file system.
+      uint32_t afid; ///< The previously obtained auth fid from attach.
+      char* uname; ///< The name of the user to attach as.
+      char* aname; ///< The name of the file tree to access.
+    } tattach; ///< The content of the Tattach message.
+
+    struct {
+      rfs_qid_t qid; ///< The qid of the root of the file tree.
+    } rattach; ///< The content of the Rattach message.
+
+    struct {
+      uint32_t fid; ///< The fid to start the walk from.
+      uint32_t newfid; ///< The fid to represent the result as.
+      uint16_t nwname; ///< The number of elements in the walk request.
+      char* wname[RFS__9P_MAXWELEM]; ///< The path elements to walk.
+    } twalk; ///< The content of the Twalk message.
+
+    struct {
+      uint16_t nwqid; ///< The number of qid values.
+      rfs_qid_t wqid[RFS__9P_MAXWELEM]; ///< The path element results.
+    } rwalk; ///< The content of the Rwalk message.
+
+    struct {
+      uint32_t fid; ///< The fid to open.
+      uint8_t mode; ///< The mode to open the file with.
+    } topen; ///< The content of the Topen message.
+
+    struct {
+      rfs_qid_t qid; ///< The qid of the file opened.
+      uint32_t iounit; ///< The max # of bytes tx w/out splitting the msg.
+    } ropen, rcreate; /// The content of the Ropen and Rcreate messages.
+
+    struct {
+      uint32_t fid; ///< The fid of the directory to create the file in.
+      char* name; ///< The name of the file to create (last entry in path).
+      uint32_t perm; ///< The permissions to give to the file.
+      uint8_t mode; ///< The mode to open the file with.
+    } tcreate; ///< The content of the Tcreate message.
+
+    struct {
+      uint32_t fid; ///< The fid of the file to read.
+      uint64_t offset; ///< The offset to read from in the file.
+      uint32_t count; ///< The number of bytes to read.
+    } tread; ///< The content of the Tread message.
+
+    struct {
+      uint32_t count; ///< The number of bytes read.
+      unsigned char* data; ///< The data read from the file.
+    } rread; ///< The contents of the Rread message.
+
+    struct {
+      uint32_t fid; ///< The fid of the file to write.
+      uint64_t offset; ///< The offset of the file to write as.
+      uint32_t count; ///< The number of bytes to write.
+      unsigned char* data; ///< The data to write to the file.
+    } twrite; ///< The contents of the Twrite message.
+
+    struct {
+      uint32_t count; ///< The number of bytes written.
+    } rwrite; ///< The contents of the Rwrite message.
+
+    struct {
+      uint32_t fid; ///< The fid of the entry to clunk, remove or stat.
+    } tclunk, tremove, tstat; ///< The contents of Tclunk, Tremove and Tstat.
+
+    // rclunk, rremove are empty
+
+    struct {
+      rfs__9p_stat_t* stat; ///< The metadata of the stat'd file.
+    } rstat; ///< The contents of the Rstat message.
+
+    struct {
+      uint32_t fid; ///< The fid of the entry to update the metadata of.
+      rfs__9p_stat_t* stat; ///< The updated metadata of the entry.
+    } twstat; ///< The contents of the Twstat message.
+
+    // rwstat is empty
+  } params;
+
+} rfs__9p_msg_t;
+
+/// @brief Initializes a stat structure.
+/// @note This will not free any allocated strings!
+/// @param [in] stat The structure to initialize.
+void rfs__9p_stat_init(rfs__9p_stat_t* stat);
+
+/// @brief Resets all values to initial, freeing any allocated strings.
+/// @note Do not call on strings you cannot free - i.e. from stat_unpack.
+/// @param [in] stat The structure to reset.
+void rfs__9p_stat_reset(rfs__9p_stat_t* stat);
+
+/// @brief Calculates the serialized size of a stat structure.
+/// @param [in] stat The structure to calculate the size of.
+/// @return The size of the structure, if serialized.
+uint16_t rfs__9p_stat_size(const rfs__9p_stat_t* stat);
+
+/// @brief Serialize the stat structure to the provided buffer.
+/// @param [in] stat The structure to serialize.
+/// @param [in] buf The buffer to serialize the stat to.
+/// @param [in] bufsize The maximum amount of data which can be written.
+/// @return The number of bytes in buf used; 0 on error.
+size_t rfs__9p_stat_pack(rfs__9p_stat_t* stat,
+                         unsigned char* buf,
+                         size_t bufsize);
+
+/// @brief Deserializes a stat structure from the provided buffer.
+/// @param [in] buf The buffer to retrieve the data from.
+/// This is modified so that the stat structure can reference included strings.
+/// @param [in] bufsize The size of the buffer.
+/// @param [in] stat The structure to deserialize into.
+/// @return The number of bytes processed from buf; 0 on error.
+size_t rfs__9p_stat_unpack(unsigned char* buf,
+                           size_t bufsize,
+                           rfs__9p_stat_t* stat);
+
+/// @brief Initializes a message structure.
+/// @note This will not free any allocated strings!
+/// @param [in] msg The structure to initialize.
+void rfs__9p_msg_init(rfs__9p_msg_t* msg);
+
+/// @brief Resets all values to initial, freeing any allocated strings.
+/// @note Do not call on strings you cannot free - i.e. from msg_unpack.
+/// @param [in] msg The structure to reset.
+void rfs__9p_msg_reset(rfs__9p_msg_t* msg);
+
+/// @brief Calculates the serialized size of a msg structure.
+/// @param [in] msg The structure to calculate the size of.
+/// @return The size of the structure, if serialized.
+uint32_t rfs__9p_msg_size(const rfs__9p_msg_t* msg);
+
+/// @brief Serialize the msg structure to the provided buffer.
+/// @param [in] msg The structure to serialize.
+/// @param [in] buf The buffer to serialize the stat to.
+/// @param [in] bufsize The maximum amount of data which can be written.
+/// @return The number of bytes in buf used; 0 on error.
+size_t rfs__9p_msg_pack(rfs__9p_msg_t* msg,
+                        unsigned char* buf,
+                        size_t bufsize);
+
+/// @brief Deserializes a message structure from the provided buffer.
+/// @param [in] buf The buffer to retrieve the data from.
+/// This is modified so that the msg structure can reference included strings.
+/// @param [in] bufsize The size of the buffer.
+/// @param [in] msg The structure to deserialize into.
+/// @return The number of bytes processed from buf; 0 on error.
+size_t rfs__9p_msg_unpack(unsigned char* buf,
+                          size_t bufsize,
+                          rfs__9p_msg_t* msg);
+
+#endif
+
